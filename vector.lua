90650918b6813212b6bae27a197cc57b1cd444d7
chocolate_bar2410 <chocolatebar24.business@gmail.com>
2026-06-03 17:26:44 +1000

    added license

--- vector.lua
+++ vector.lua
@@ -1,3 +1,12 @@
+--[[
+
+Copyright (c) 2026 chocolate_bar2410
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of the MIT license. See LICENSE for details.
+
+]]
+
 local schema = {}
 local interface = setmetatable({},{
     __index = function (self, index)
