bd2685d1e7a98dc3d19f98ecfd060aaaaa56266c
chocolate_bar2410 <chocolatebar24.business@gmail.com>
2026-06-08 12:44:00 +1000

    added iterator

--- queue.lua
+++ queue.lua
@@ -1,5 +1,7 @@
 local interface = {}
-local schema = {}
+local schema = {
+    _OFFSET_THRESHOLD = 10
+}
 local meta = {
     __index = schema,
     __tostring = function(self)
@@ -9,7 +11,7 @@ local meta = {
 
         local result = ""
         for i = self.min,self.max do
-            result = result .. self.data[i] .. " "
+            result = result .. tostring(self.data[i]) .. " "
         end
 
         result = result:sub(1,result:len() - 1)
@@ -27,8 +29,6 @@ interface.new = function()
     queue.max = 0
     queue.data = {}
 
-    queue._OFFSET_THRESHOLD = 10
-
     return setmetatable(queue,meta)
 end
 
@@ -42,10 +42,9 @@ function schema:pop()
     self.data[self.min] = nil
     self.min = self.min + 1
 
-    if self.min > self._OFFSET_THRESHOLD then
+    if self.min > schema._OFFSET_THRESHOLD then
         self:_compact()
     end
-
 
     return result
 end
@@ -57,7 +56,6 @@ end
 function schema:push(value)
     self.max = self.max + 1
     self.data[self.max] = value
-    
 end
 
 ---returns front item of queue
@@ -72,7 +70,7 @@ end
 ---returns size of queue
 ---@generic T
 ---@param self queue<T>
----@return T?
+---@return number?
 function schema:size()
     return math.max(self.max - self.min + 1,0)
 end
@@ -80,7 +78,7 @@ end
 ---returns true/false depending on if the queue is empty
 ---@generic T
 ---@param self queue<T>
----@return T?
+---@return boolean?
 function schema:empty()
     return self.max - self.min < 0
 end
@@ -88,16 +86,38 @@ end
 ---compacts queue into a smaller size
 ---@generic T
 ---@param self queue<T>
+---@private
 function schema:_compact()
     if self:empty() then return end
     local size = self:size()
 
+    local new_data = {}
+
     for i = self.min, self.max do
-        self.data[i - self.min + 1] = self.data[i]
-        self.data[i] = nil
+        table.insert(new_data, self.data[i])
     end
+
     self.min = 1
     self.max = size
+    self.data = new_data
+end
+
+function schema:iterate()
+    local index = 0
+    local size = self:size()
+
+    local min = self.min
+    local max = self.max
+
+    return function()
+        local current = index + min
+        if current > max then return end
+        local value = self.data[current]
+
+        index = index + 1
+
+        return index,value
+    end
 end
 
 ---@generic T
@@ -111,7 +131,8 @@ end
 ---@field peek fun(self : queue<T>): T?
 ---@field size fun(self : queue<T>): number
 ---@field empty fun(self : queue<T>): boolean
----@field _compact fun(self : queue<T>)
+---@field private _compact fun(self : queue<T>)
+---@field iterate fun(self : queue<T>): fun(): number,T
 
 
 return interface
